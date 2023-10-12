2ade4434a3964ec63167e8e3fbb2cdbb8be3a202
zonglab <44907440+zonglab@users.noreply.github.com>
2023-10-11 23:15:27 -0500

    Add files via upload

---
+++ scripts/barcodeseq_extract_standard_run.py
@@ -0,0 +1,31 @@
+
+import sys
+import pysam
+
+# input file 1: read2 fastq
+# input file 2: reads file
+# input 3: read length of trimmed read 2
+# read_id gene_id barcode
+
+read_fi = open(sys.argv[2], 'r')
+read_dict = {}	# read_id: {'gene_id':, 'bcd':}
+
+for read_line in read_fi:
+	read_id, gene_id = read_line.strip('\n').split(' ')
+	read_dict[read_id] = {'Gene': gene_id, 'Barcode': ''}
+read_fi.close()
+
+del read_line
+del read_id
+del gene_id
+
+fastq_fi = pysam.FastqFile(sys.argv[1])
+for read in fastq_fi:
+	if (len(read.sequence) == int(sys.argv[3])) and (read.sequence[0:8].count('N') < 4):
+		read_dict[read.name]['Barcode'] = read.sequence[0:8]
+fastq_fi.close()
+del read
+
+for read in read_dict:
+	if len(read_dict[read]['Barcode']) == 8:
+		print(read, read_dict[read]['Gene'], read_dict[read]['Barcode'])
